25577a8f2cab30d9a0c486969f0dde303e8a4dac
varanitsa <varanitsa.es@gmail.com>
2023-12-25 21:41:37 +0100

    added validation to handle Bad Request in @PostMapping, fix constructor to the Movie.class

--- src/main/java/com/redpoints/interview/model/Movie.java
+++ src/main/java/com/redpoints/interview/model/Movie.java
@@ -1,10 +1,28 @@
 package com.redpoints.interview.model;
 
+
 public class Movie {
+
     private Long id;
     private String title;
     private String director;
     private int year;
+
+    public Movie(Long id, String title, String director, int year) {
+        this.id = id;
+        this.title = title;
+        this.director = director;
+        this.year = year;
+    }
+
+    public Movie() {
+    }
+
+    public Movie(String title, String director, int year) {
+        this.title = title;
+        this.director = director;
+        this.year = year;
+    }
 
     public Long getId() {
         return id;
